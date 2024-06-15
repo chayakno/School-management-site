2de4cb1fc152a60e22818a8640b6874b07d5142d
rachely1234 <rachelB2855@gmail.com>
2024-06-15 23:26:46 +0300

    swagger

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { swaggerUi, specs } = require('./swagger'); 
 const app = express();
 
-// קישור למאגר הנתונים
-const mongoURI = 'mongodb://127.0.0.1:27017/School-management'; // החלף בשם מאגר הנתונים שלך
+
+const mongoURI = 'mongodb://127.0.0.1:27017/School-management';
 mongoose.connect(mongoURI).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('Error connecting to MongoDB', err);
 });
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
 
-// הגדרת השרת
+
 app.listen(5000, function() {
     console.log('Server running on port 5000');
 });
@@ -20,7 +26,7 @@ app.get('/', function(req, res) {
 });
 
 const User = require('./models/user.Schema');
-// const Student = require('./models/student');
+
 
 // דוגמא ליצירת משתמש וסטודנט
 app.post('/create-user', async (req, res) => {
