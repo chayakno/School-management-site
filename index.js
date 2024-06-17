8fa53601aa614499ff4df9d97b6b6e473ee4ae6c
rachely1234 <rachelB2855@gmail.com>
2024-06-17 16:44:00 +0300

    changes on index page

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const studRauter = require("./src/routes/student.rout");
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
 
-const config=require('dotenv').config();
-const mongoURI = 'mongodb://127.0.0.1:27017/School-management';
-mongoose.connect(mongoURI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB', err);
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/students", studRauter);
 
-
-app.listen(5000, function () {
-    console.log('Server running on port 5000');
-});
-studentRouter = require('./src/routes/student.rout'); 
- app.use('/students', studentRouter);
-
-app.get('/', function (req, res) {
-    res.send('Hello');
-});
+const PORT =  5000;
+mongoose.connect('mongodb://127.0.0.1:27017/School-management',{}).then(
+    ()=>app.listen(PORT,()=>console.log(`server runing on port ${PORT}`)))
+    .catch((error)=>console.log(error.message));
 
 const Student=require('./src/models/student.Schema');
 
