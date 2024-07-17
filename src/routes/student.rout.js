e5fe185d48d8c1ca881ec471871e5ec69ae388a0
rachely1234 <rachelB2855@gmail.com>
2024-07-17 16:38:00 +0300

    update folders

--- src/routes/student.rout.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const {addStudent,getAllStudents,getAllPendingStudents} = require('../controllers/student/student.controller');
-router.post('/add', addStudent);
-router.get('/getAllStudents', getAllStudents);
-router.get('/getAllPendingStudents', getAllPendingStudents);
-
-module.exports = router;
--- src/routes/user.rout.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const usersController = require('../controllers/User/user.controller');
-
-// router.put('/users/:userId/status', usersController.updateUserStatus);
-
-
-
-// module.exports = router;
